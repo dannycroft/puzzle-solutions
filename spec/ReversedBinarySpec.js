5ddeeda891879eba5179f68b4246a421f7d77cf2
dannycroft <danny.croft@yahoo.co.uk>
2013-06-20 13:25:08 +0100

    Adding specs for reversed binary

--- spec/ReversedBinarySpec.js
+++ spec/ReversedBinarySpec.js
@@ -4,6 +4,23 @@ describe("Puzzle collection: ", function() {
 
     describe("Reversed Binary", function() {
 
+        it("should have a convert prototype function", function() {
+            expect(typeof rb["convert"]).toBe("function");
+        });
+
+        it("should return the correct value", function() {
+            expect(rb.convert(13)).toEqual(11);
+            expect(rb.convert(47)).toEqual(61);
+        });
+
+        it("should handle string based integers", function() {
+            expect(rb.convert("13")).toEqual(11);
+            expect(rb.convert("47")).toEqual(61);
+        });
+
+        it("should handle non-integer types", function() {
+            expect(rb.convert("abc")).toBeUndefined();
+        });
 
     });
 });
