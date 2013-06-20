576128e2e293046f22b5e5a852bab13042132daf
dannycroft <danny.croft@yahoo.co.uk>
2013-06-20 15:07:53 +0100

    Adding stricture parseInt function

--- app/ReversedBinary.js
+++ app/ReversedBinary.js
@@ -6,7 +6,16 @@
 
 module.exports = new ReverseBinary();
 
-function ReverseBinary() {}
+function ReverseBinary() {
+
+    // Stricter parse function
+    this.filterInt = function(value) {
+        if(/^\-?([0-9]+|Infinity)$/.test(value)) {
+            return Number(value);
+        }
+        return NaN;
+    };
+}
 
 /**
  * Convert n to a binary number and return it's
@@ -15,7 +24,10 @@ function ReverseBinary() {}
  * @return {Number}
  */
 ReverseBinary.prototype.convert = function(n) {
+    n = this.filterInt(n);
+    if (!isNaN(n)) {
         return parseInt(n.toString(2)
                 .split("").reverse("")
                 .join("").toString(2), 2);
+    }
 };
