6aee5b265c6d49588e4ef3f8a1da1fe31e3d4bb5
dannycroft <danny.croft@yahoo.co.uk>
2013-06-20 13:32:46 +0100

    Adding conversion prototype to reverse binary

--- app/ReversedBinary.js
+++ app/ReversedBinary.js
@@ -7,3 +7,15 @@
 module.exports = new ReverseBinary();
 
 function ReverseBinary() {}
+
+/**
+ * Convert n to a binary number and return it's
+ * reversed binary representation
+ * @param  {Number} n
+ * @return {Number}
+ */
+ReverseBinary.prototype.convert = function(n) {
+        return parseInt(n.toString(2)
+                .split("").reverse("")
+                .join("").toString(2), 2);
+};
