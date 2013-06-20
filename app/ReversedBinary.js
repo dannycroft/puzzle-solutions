25b604951fc41021fe7e663bdf8234a77afbcd77
dannycroft <danny.croft@yahoo.co.uk>
2013-06-20 13:21:08 +0100

    Initial commit

---
+++ app/ReversedBinary.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a new ReverseBinary instance
+ * @constructor
+ * @see https://www.spotify.com/uk/jobs/tech/reversed-binary/
+ */
+
+module.exports = new ReverseBinary();
+
+function ReverseBinary() {}
---
+++ spec/ReversedBinarySpec.js
@@ -0,0 +1,9 @@
+var rb = require("../app/ReversedBinary.js");
+
+describe("Puzzle collection: ", function() {
+
+    describe("Reversed Binary", function() {
+
+
+    });
+});
